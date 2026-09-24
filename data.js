ab2330825726e96f984539fe42a51006be5f98ce
kotodamaer <18118013948@163.com>
2026-09-24 22:32:04 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,165 @@
+var articlesData = {
+      redglove: {
+        name: '红手套',
+        subtitle: '"成为砝码，成为火炬，成为循环不息的雨"',
+        list: [
+          { title: '第一章 · 归巢by水母', href: '红手套1.html' },
+          { title: '第二章 · 初拥by雾Euro', href: '红手套2.html' },
+          { title: '第三章 · 红手套的惩罚by水母', href: '红手套3.html' },
+          { title: '第四章 · 礼物', href: '红手套4.html' },
+          { title: '第五章 · 木马 byfantasma', href: '红手套5.html' },
+          { title: '第六章 · 请为西西莉亚开枝散叶吧！8by水母', href: '红手套6.html' }
+        ]
+      },
+  wolf: {
+    name: '野狼',
+    subtitle: '"我不想要自由，我只想要她"',
+    list: [
+      { title: '第一章 · 请为西西里亚开枝散叶吧！by水母', href: '野狼1.html' }
+    ]
+  },
+  ghost: {
+    name: '幽灵',
+    subtitle: '"欢迎回家boss，您是想先用餐，还是先洗浴？"',
+    list: [
+      { title: '第一章 · 请为西西莉亚开枝散叶吧！by水母', href: '幽灵1.html' },
+      { title: '第二章 · 舞会by水母', href: '幽灵2.html' }
+    ]
+  },
+  puppeteer: {
+    name: '提线人',
+    subtitle: '"让坐轮椅的上战场？你脑子有病吧"',
+    list: [
+      { title: '第一章 · 请为西西莉亚开枝散叶吧！by水母', href: '提线人1.html' },
+      { title: '第二章 · 你x提线人by常安', href: '提线人2.html' },
+      { title: '第三章 · 你x提线人by 老中医', href: '提线人3.html' },
+      { title: '第四章 · 你x提线人by凤梨酥', href: '提线人4.html' },
+      { title: '第五章 · boss，我爬不动了byfantasma', href: '提线人5.html' },
+      { title: '第六章 · 你x提线人·走绳 中医', href: '提线人6.html' },
+      { title: '第七章 · 水煎好文明', href: '提线人7.html' }
+    ]
+  },
+  tyrant: {
+    name: '暴君',
+    subtitle: '"这里是泰拉，我是泰拉的暴君"',
+    list: [
+      { title: '第一章 · 请为西西莉亚开枝散叶吧！by水母', href: '暴君1.html' }
+    ]
+  },
+  nobody: {
+    name: '某某人',
+    subtitle: '"我是你的某某人"',
+    list: [
+      { title: '第一章 · 请为西西莉亚开枝散叶吧！by水母', href: '某某人1.html' }
+    ]
+  },
+  chef: {
+    name: '主厨',
+    subtitle: '"我是桑托罗餐厅的主厨"',
+    list: [
+      { title: '第一章 · 主厨（春梦）by宽粉', href: '主厨1.html' },
+      { title: '第二章 · 请为西西利亚开枝散叶吧6by水母', href: '主厨2.html' }
+    ]
+  },
+  fool: {
+    name: '愚者',
+    subtitle: '"这世上没有比我对命运更忠诚的人了"',
+    list: [
+      { title: '第一章 · 论杯子的养成与护理 by水母', href: '愚者1.html' }
+    ]
+  },
+  general: {
+    name: '将军',
+    subtitle: '"我会帮你重建西西利亚，帮你扫清一切阻碍"',
+    list: [
+      { title: '第一章 · 将军（春梦）by宽粉', href: '将军1.html' },
+      { title: '第二章 · 你x将军by水母', href: '将军2.html' },
+      { title: '第三章 · 将军·请为西西莉亚开枝散叶吧！7by水母', href: '将军3.html' }
+    ]
+  },
+  windward: {
+    name: '东南西北',
+    subtitle: '"这一切只是逢场作戏，佳期一梦吗？"',
+    list: [
+      { title: '第一章 · 东南西北gb by困困困困', href: '东南西北1.html' },
+      { title: '第二章 · 七夕福利by水母', href: '东南西北2.html' },
+      { title: '第三章 · 你x东南西北·青梅酒by水母', href: '东南西北3.html' },
+     ]
+  },
+  endy: {
+    name: '九命猫',
+    subtitle: '"你想要的我都可以给你！"',
+    list: [
+      { title: '第一章 · 九命猫（春梦）by宽粉', href: '九命猫1.html' }
+    ]
+  },
+  interviewer: {
+    name: '面试官',
+    subtitle: '"恭喜你，你被录取了"',
+    list: [
+      { title: '第一章 · 面试官（春梦）by宽粉', href: '面试官1.html' },
+      { title: '第二章 · 面试官（纯爱）by水母', href: '面试官2.html' },
+      { title: '第三章 · 画笔by水母', href: '面试官3.html' },
+      { title: '第四章 · NG也会吃醋吗？by水母', href: '面试官4.html' },
+      { title: '第五章 · 涟漪by水母', href: '面试官5.html' },
+      { title: '第六章 · 未命名by水母', href: '面试官6.html' }
+    ]
+  },
+  rosasr: {
+    name: '罗萨斯',
+    subtitle: '"我恨你，但我更恨你不爱我"',
+    list: [
+      { title: '第一章 · 戒断by水母', href: '罗萨斯1.html' },
+      { title: '第二章 · 成瘾性by水母', href: '罗萨斯2.html' },
+      { title: '第三章 · 春梦1by宽粉', href: '罗萨斯3.html' },
+      { title: '第四章 · 春梦2by宽粉', href: '罗萨斯4.html' }
+    ]
+  },
+  liquidator: {
+    name: '刀锋',
+    subtitle: '"收到，请告诉我任务目标"',
+    list: [
+      { title: '第一章 · 回雁by水母', href: '刀锋1.html' }
+    ]
+  },
+  heishao: {
+    name: '黑哨',
+    subtitle: '"除了你，我现在不想听见任何动静"',
+    list: [
+      { title: '第一章 · 蓝莓慕斯by鸣折', href: '黑哨1.html' },
+      { title: '第二章 · 感官过载by水母', href: '黑哨2.html' }
+    ]
+  },
+  shengzi: {
+    name: '圣子',
+    subtitle: '"我需要你成为我的半身"',
+    list: [
+      { title: '第一章 · 圣子是好男孩吗？by水母', href: '圣子1.html' }
+    ]
+  },
+  NP: {
+    name: 'NP',
+    subtitle: '"np小剧场，注意甄别"',
+    list: [
+      { title: '第一章 · [你x暴君+刀锋]boss我们这样真的好吗by水母', href: 'np1.html' },
+      { title: '第二章 · 一次单纯的旅行（1vs5）by雾Euro', href: 'np2.html' },
+      { title: '第三章 · [你x幽灵+面试官]zc什么的还是放一边吧by水母', href: 'np3.html' },
+      { title: '第四章 · [你x面试官+愚者]肝帝的一百种使用方式by水母', href: 'np4.html' }
+    ]
+  },
+  kaiye: {
+    name: '请为西西莉亚开枝散叶吧',
+    subtitle: '"开枝散叶系列合集"',
+    list: [
+      { title: '第一章 · 请为西西莉亚开枝散叶吧！1', href: '开枝散叶1.html' },
+      { title: '第二章 · 请为西西莉亚开枝散叶吧！2', href: '开枝散叶2.html' },
+      { title: '第三章 · 请为西西莉亚开枝散叶吧！3', href: '开枝散叶3.html' },
+      { title: '第四章 · 请为西西莉亚开枝散叶吧！4', href: '开枝散叶4.html' },
+      { title: '第五章 · 请为西西莉亚开枝散叶吧！5', href: '开枝散叶5.html' },
+      { title: '第六章 · 请为西西莉亚开枝散叶吧！6', href: '开枝散叶6.html' },
+      { title: '第七章 · 请为西西莉亚开枝散叶吧！7', href: '开枝散叶7.html' },
+      { title: '第八章 · 请为西西莉亚开枝散叶吧！8', href: '开枝散叶8.html' },
+      { title: '第九章 · 请为西西莉亚开枝散叶吧！9', href: '开枝散叶9.html' }
+    ]
+  }
+};
